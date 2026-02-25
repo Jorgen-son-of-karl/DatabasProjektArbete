3feedb4690973486d6e7611d35ee559116df5237
Jörgen <jorgen.karlsson.91@gmail.com>
2026-02-25 09:06:55 +0100

    added audio equipment

---
+++ src/main/java/com/wigell/entity/AudioEquipment.java
@@ -0,0 +1,72 @@
+package com.wigell.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "audio_equipment")
+public class AudioEquipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category", nullable = false,  length = 50)
+    private String category;
+
+    @Column(name = "brand", nullable = false,  length = 50)
+    private String brand;
+
+    @Column(name = "model", nullable = false,  length = 50)
+    private String model;
+
+    @Column(name = "price_per_day", nullable = false)
+    private double pricePerDay;
+
+
+    protected AudioEquipment() {}
+
+    public AudioEquipment(Long id, String name, String category, String brand, String model, double pricePerDay) {
+        this.id = id;
+        this.category = category;
+        this.brand = brand;
+        this.model = model;
+        this.pricePerDay = pricePerDay;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+}
