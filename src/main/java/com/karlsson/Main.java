0b4c849ffd92dd70eb2cb1a7f668f22177fc6ef0
Jörgen <jorgen.karlsson.91@gmail.com>
2026-03-11 10:29:50 +0100

    member CRUD

--- src/main/java/com/karlsson/Main.java
+++ src/main/java/com/karlsson/Main.java
@@ -1,14 +1,53 @@
 package com.karlsson;
 
+import com.karlsson.entity.Member;
+import com.karlsson.repo.MemberRepository;
+import com.karlsson.repo.MemberRepositoryImpl;
+import com.karlsson.service.MemberService;
 import com.karlsson.util.HibernateUtil;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        HibernateUtil.getSessionFactory();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
+
+
+
+        MemberRepository memberRepository = new MemberRepositoryImpl(sessionFactory);
+
+        MemberService memberService = new MemberService(memberRepository);
         System.out.println("Hibernate started!");
+
+
+//        memberService.createMember("Perkele", "hallå");
+
+        Member m = memberService.getMember(3L);
+        System.out.println(m.getName() + " " + m.getEmail());
+
+        List<Member> members = memberService.getAllMembers();
+        for (Member member : members) {
+            System.out.println(member.getName());
+        }
+
+        memberService.deleteMember(m);
+
+        members = memberService.getAllMembers();
+        for (Member member : members) {
+            System.out.println(member.getName());
+        }
+
+//        m.setName("Arne Karlsson");
+//        memberService.updateMember(m);
+//
+//        m = memberService.getMember(1L);
+//        System.out.println(m.getName() + " " + m.getEmail());
+
+
 
     }
 }
--- src/main/java/com/karlsson/service/MemberService.java
+++ src/main/java/com/karlsson/service/MemberService.java
@@ -3,17 +3,38 @@ package com.karlsson.service;
 import com.karlsson.entity.Member;
 import com.karlsson.repo.MemberRepository;
 
+import java.util.List;
+
 public class MemberService {
 
     private final MemberRepository memberRepository;
     public MemberService(MemberRepository memberRepository) { this.memberRepository = memberRepository; }
 
-    public Member createMember(String name, String email) {
-        if(name == null || name.isBlank()) throw new IllegalArgumentException("Namn krävs.");
-        if (email == null || email.isBlank()) throw new IllegalArgumentException("Email krävs.");
+    public void createMember(String name, String email) {
+        if(name == null || name.isBlank()) throw new IllegalArgumentException("Name cannot be null or blank");
+        if (email == null || email.isBlank()) throw new IllegalArgumentException("Email cannot be null or blank.");
 
         Member member = new Member(name.trim(), email.trim());
         memberRepository.save(member);
-        return member;
+    }
+
+    public Member getMember(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public List<Member> getMembers() {
+        return memberRepository.findAll();
+    }
+
+    public void updateMember(Member member) {
+        memberRepository.update(member);
+    }
+
+    public void deleteMember(Member member) {
+        memberRepository.delete(member);
     }
 }
