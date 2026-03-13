87c22199ba607599d2b4bf067a7608a1be7d242b
Jörgen <jorgen.karlsson.91@gmail.com>
2026-03-13 10:19:49 +0100

    crud for instrument

--- src/main/java/com/karlsson/Main.java
+++ src/main/java/com/karlsson/Main.java
@@ -20,8 +20,6 @@ public class Main {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
 
-
-
         MemberRepository memberRepository = new MemberRepositoryImpl(sessionFactory);
         InstrumentRepository instrumentRepository = new InstrumentRepositoryImpl(sessionFactory);
 
@@ -35,35 +33,43 @@ public class Main {
 
         while (running) {
             System.out.println();
-        System.out.println("Please select what you want to do from the list bellow: ");
-        System.out.println("1. List all members");
-        System.out.println("2. Create a new member");
-        System.out.println("3. Update existing member");
-        System.out.println("4. Delete existing member");
+            System.out.println("Please select what you want to do from the list bellow: ");
+            System.out.println("1. List all members");
+            System.out.println("2. Create a new member");
+            System.out.println("3. Update existing member");
+            System.out.println("4. Delete existing member");
             System.out.println("5. List all instruments");
             System.out.println("6. Add a new instrument to stock");
-        Scanner userInput = new Scanner(System.in);
+            System.out.println("7. Update existing instrument");
+            System.out.println("8. Delete existing instrument");
+            Scanner userInput = new Scanner(System.in);
 
-            switch (userInput.nextInt()) {
-                case 1:
-                    memberService.listAllMembers();
-                    break;
-                case 2:
-                    memberService.createMember(userInput);
-                    break;
-                case 3:
-                    memberService.updateMember(userInput);
-                    break;
-                case 4:
-                    memberService.deleteMember(userInput);
-                    break;
-                case 5:
-                    instrumentService.listAllInstruments();
-                    break;
+                switch (userInput.nextInt()) {
+                    case 1:
+                        memberService.listAllMembers();
+                        break;
+                    case 2:
+                        memberService.createMember(userInput);
+                        break;
+                    case 3:
+                        memberService.updateMember(userInput);
+                        break;
+                    case 4:
+                        memberService.deleteMember(userInput);
+                        break;
+                    case 5:
+                        instrumentService.listAllInstruments();
+                        break;
                     case 6:
                         instrumentService.createInstrument(userInput);
                         break;
-            }
+                    case 7:
+                        instrumentService.updateInstrument(userInput);
+                        break;
+                    case 8:
+                        instrumentService.deleteInstrument(userInput);
+                        break;
+                }
         }
     }
 }
